d835895e248241f9a68d6f793cae43bf69cf5e12
harsh674 <1829058@kiit.ac.in>
2020-07-22 22:31:47 +0530

    Added Home Page With Nav Bar

---
+++ lib/category.dart
@@ -0,0 +1,21 @@
+
+class Category {
+  final String name;
+  final int numOfCourses;
+  final String image;
+
+  Category(this.name, this.numOfCourses, this.image);
+}
+
+List<Category> categories = categoriesData
+    .map((item) => Category(item['name'], item['courses'], item['image']))
+    .toList();
+
+var categoriesData = [
+  {"name": "Development", 'courses': 17, 'image': "assets/images/Development.png"},
+  {"name": "Design", 'courses': 25, 'image': "assets/images/Design.jpg"},
+  {"name": "Programming", 'courses': 13, 'image': "assets/images/Programming.jpg"},
+  {"name": "IT and Software", 'courses': 17, 'image': "assets/images/IT.jpg"},
+
+
+];
