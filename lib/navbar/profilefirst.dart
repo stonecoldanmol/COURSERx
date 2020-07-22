89197627fea751db5553e9bfacc38606696699d2
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-07-23 00:30:12 +0530

    Resolved the orientation problem in profile section.

--- lib/navbar/profilefirst.dart
+++ lib/navbar/profilefirst.dart
@@ -43,205 +43,207 @@ class _ProfileFirstState extends State<ProfileFirst> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffF8F8FA),
-      body: Stack(
-        overflow: Overflow.visible,
-        children: <Widget>[
-          Container(
-            color: Colors.indigoAccent,
-            height: 40 * SizeConfig.heightMultiplier,
-            child: Padding(
-              padding:  EdgeInsets.only(left: 30.0, right: 30.0, top: 10 * SizeConfig.heightMultiplier),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Container(
-                        height: 11 * SizeConfig.heightMultiplier,
-                        width: 22 * SizeConfig.widthMultiplier,
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: AssetImage("assets/profileimg.png"))
-                        ),
-                      ),
-                      SizedBox(width: 5 * SizeConfig.widthMultiplier,),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text("My profile", style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 3 * SizeConfig.textMultiplier,
-                              fontWeight: FontWeight.bold
-                          ),),
-                          SizedBox(height: 1 * SizeConfig.heightMultiplier,),
-                          Row(
-                            children: <Widget>[
-                              Row(
-                                children: <Widget>[
-                                  Image.asset(
-                                    "assets/fb.png",
-                                    height: 3 * SizeConfig.heightMultiplier,
-                                    width: 3 * SizeConfig.widthMultiplier,
-                                    color: Colors.white70,
-                                  ),
-                                  SizedBox(width: 2 * SizeConfig.widthMultiplier,),
-                                  Text("Facebook", style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 1.5 * SizeConfig.textMultiplier,
-                                  ),),
-                                ],
-                              ),
-                              SizedBox(width: 7 * SizeConfig.widthMultiplier,),
-                              Row(
-                                children: <Widget>[
-                                  Image.asset(
-                                    "assets/insta.png",
-                                    height: 3 * SizeConfig.heightMultiplier,
-                                    width: 3 * SizeConfig.widthMultiplier,
-                                    color: Colors.white70,
-                                  ),
-                                  SizedBox(width: 2 * SizeConfig.widthMultiplier,),
-                                  Text("Instagram", style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 1.5 * SizeConfig.textMultiplier,
-                                  ),),
-                                ],
-                              )
-                            ],
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 3 * SizeConfig.heightMultiplier,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          Text("10.2K", style: TextStyle(
-                              color: Colors.white70,
-                              fontSize: 3 * SizeConfig.textMultiplier,
-                              fontWeight: FontWeight.bold
-                          ),),
-                          Text("Followers", style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 1.9 * SizeConfig.textMultiplier,
-                          ),),
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Text("543", style: TextStyle(
-                              color: Colors.white70,
-                              fontSize: 3 * SizeConfig.textMultiplier,
-                              fontWeight: FontWeight.bold
-                          ),),
-                          Text("Points", style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 1.9 * SizeConfig.textMultiplier,
-                          ),),
-                        ],
-                      ),
-
-                      GestureDetector(
-                        onTap: (){
-
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.white70),
-                            borderRadius: BorderRadius.circular(5.0),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("EDIT PROFILE",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 1.8 * SizeConfig.textMultiplier,
-                            ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-
-
-          Padding(
-            padding:  EdgeInsets.only(top: 35 * SizeConfig.heightMultiplier),
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(30.0),
-                    topLeft: Radius.circular(30.0),
-                  )
-              ),
-              child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Stack(
+          overflow: Overflow.visible,
+          children: <Widget>[
+            Container(
+              color: Colors.indigoAccent,
+              height: 40 * SizeConfig.heightMultiplier,
+              child: Padding(
+                padding:  EdgeInsets.only(left: 30.0, right: 30.0, top: 10 * SizeConfig.heightMultiplier),
                 child: Column(
                   children: <Widget>[
-                    Padding(
-                      padding:  EdgeInsets.only(left: 30.0, top: 3 * SizeConfig.heightMultiplier),
-                      child: Text("My Courses", style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 2.2 * SizeConfig.textMultiplier
-                      ),),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          height: 11 * SizeConfig.heightMultiplier,
+                          width: 22 * SizeConfig.widthMultiplier,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: AssetImage("assets/profileimg.png"))
+                          ),
+                        ),
+                        SizedBox(width: 5 * SizeConfig.widthMultiplier,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text("My profile", style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 3 * SizeConfig.textMultiplier,
+                                fontWeight: FontWeight.bold
+                            ),),
+                            SizedBox(height: 1 * SizeConfig.heightMultiplier,),
+                            Row(
+                              children: <Widget>[
+                                Row(
+                                  children: <Widget>[
+                                    Image.asset(
+                                      "assets/fb.png",
+                                      height: 3 * SizeConfig.heightMultiplier,
+                                      width: 3 * SizeConfig.widthMultiplier,
+                                      color: Colors.white70,
+                                    ),
+                                    SizedBox(width: 2 * SizeConfig.widthMultiplier,),
+                                    Text("Facebook", style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 1.5 * SizeConfig.textMultiplier,
+                                    ),),
+                                  ],
+                                ),
+                                SizedBox(width: 7 * SizeConfig.widthMultiplier,),
+                                Row(
+                                  children: <Widget>[
+                                    Image.asset(
+                                      "assets/insta.png",
+                                      height: 3 * SizeConfig.heightMultiplier,
+                                      width: 3 * SizeConfig.widthMultiplier,
+                                      color: Colors.white70,
+                                    ),
+                                    SizedBox(width: 2 * SizeConfig.widthMultiplier,),
+                                    Text("Instagram", style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 1.5 * SizeConfig.textMultiplier,
+                                    ),),
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
                     ),
                     SizedBox(height: 3 * SizeConfig.heightMultiplier,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Text("10.2K", style: TextStyle(
+                                color: Colors.white70,
+                                fontSize: 3 * SizeConfig.textMultiplier,
+                                fontWeight: FontWeight.bold
+                            ),),
+                            Text("Followers", style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 1.9 * SizeConfig.textMultiplier,
+                            ),),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text("543", style: TextStyle(
+                                color: Colors.white70,
+                                fontSize: 3 * SizeConfig.textMultiplier,
+                                fontWeight: FontWeight.bold
+                            ),),
+                            Text("Points", style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 1.9 * SizeConfig.textMultiplier,
+                            ),),
+                          ],
+                        ),
 
-                    SizedBox(height: 3 * SizeConfig.heightMultiplier,),
-                    Padding(
-                      padding:  EdgeInsets.only(left: 30.0, right: 30.0),
-                      child: Row(
-                        children: <Widget>[
-                          Text("Bookmark Courses", style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 2.2 * SizeConfig.textMultiplier
-                          ),),
-                          Spacer(),
-                          Text("View All", style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 1.7 * SizeConfig.textMultiplier
-                          ),),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 3 * SizeConfig.heightMultiplier,),
+                        GestureDetector(
+                          onTap: (){
 
-                    SizedBox(height: 3 * SizeConfig.heightMultiplier,),
-                    Padding(
-                      padding:  EdgeInsets.only(left: 30.0, right: 30.0),
-                      child: Row(
-                        children: <Widget>[
-                          Text("Courses History", style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 2.2 * SizeConfig.textMultiplier
-                          ),),
-                          Spacer(),
-                          Text("View All", style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 1.7 * SizeConfig.textMultiplier
-                          ),),
-                        ],
-                      ),
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white70),
+                              borderRadius: BorderRadius.circular(5.0),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text("EDIT PROFILE",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 1.8 * SizeConfig.textMultiplier,
+                              ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
               ),
             ),
-          )
 
-        ],
+
+            Padding(
+              padding:  EdgeInsets.only(top: 35 * SizeConfig.heightMultiplier),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(30.0),
+                      topLeft: Radius.circular(30.0),
+                    )
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding:  EdgeInsets.only(left: 30.0, top: 3 * SizeConfig.heightMultiplier),
+                        child: Text("My Courses", style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 2.2 * SizeConfig.textMultiplier
+                        ),),
+                      ),
+                      SizedBox(height: 3 * SizeConfig.heightMultiplier,),
+
+                      SizedBox(height: 3 * SizeConfig.heightMultiplier,),
+                      Padding(
+                        padding:  EdgeInsets.only(left: 30.0, right: 30.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Bookmark Courses", style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 2.2 * SizeConfig.textMultiplier
+                            ),),
+                            Spacer(),
+                            Text("View All", style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 1.7 * SizeConfig.textMultiplier
+                            ),),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 3 * SizeConfig.heightMultiplier,),
+
+                      SizedBox(height: 3 * SizeConfig.heightMultiplier,),
+                      Padding(
+                        padding:  EdgeInsets.only(left: 30.0, right: 30.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Courses History", style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 2.2 * SizeConfig.textMultiplier
+                            ),),
+                            Spacer(),
+                            Text("View All", style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 1.7 * SizeConfig.textMultiplier
+                            ),),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+
+          ],
+        ),
       ),
 
     );
