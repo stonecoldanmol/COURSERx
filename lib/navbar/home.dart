24aaa3ca885fbda6ff49c5f7fde82fb4d1ac4665
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-07-24 23:15:23 +0530

    Updated the home screen

--- lib/navbar/home.dart
+++ lib/navbar/home.dart
@@ -1,5 +1,7 @@
+import 'package:coursesapp/Animation/FadeAnimation.dart';
 import 'package:coursesapp/constants.dart';
 import 'package:coursesapp/category.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -12,8 +14,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Courserx',
-      theme: ThemeData(),
       home: HomeScreen(),
     );
   }
@@ -23,103 +23,118 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.only(left: 20, top: 50, right: 20),
+      backgroundColor: Colors.grey[100],
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                SvgPicture.asset("assets/icons/menu.svg"),
-                Center(
-                    child: Text(
-                  "Home",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0),
-                )),
-                Container(width: 50.0),
-              ],
-            ),
-            SizedBox(height: 30),
-            Center(
-              child: Text("COURSERx,", style: kHeadingextStyle),
-            ),
-            Center(
-              child: Text("At Your Service", style: kSubheadingextStyle),
-            ),
             Container(
-              margin: EdgeInsets.symmetric(vertical: 30),
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-              height: 60,
-              width: double.infinity,
+              height: 300,
               decoration: BoxDecoration(
-                color: Color(0xFFF5F5F7),
-                borderRadius: BorderRadius.circular(40),
+                image: DecorationImage(
+                  image: AssetImage(
+                      'assets/images/background.jpg',
+                ),
+                  fit: BoxFit.cover
+                ),
               ),
-              child: Row(
-                children: <Widget>[
-                  SvgPicture.asset("assets/icons/search.svg"),
-                  SizedBox(width: 16),
-                  Text(
-                    "Search for Courses",
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Color(0xFFA0A5BD),
-                    ),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.bottomRight,
+                    colors: [
+                      Colors.black.withOpacity(0.8),
+                      Colors.black.withOpacity(0.2),
+                    ]
                   )
-                ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    FadeAnimation(1, Text("Find Your Passion!", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),)),
+                    SizedBox(height: 30,),
+                    FadeAnimation(1.3, Container(
+                      padding: EdgeInsets.symmetric(vertical: 3),
+                      margin: EdgeInsets.symmetric(horizontal: 40),
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon: Icon(Icons.search, color: Colors.grey,),
+                            hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
+                            hintText: "Search for courses, subjects ..."
+                        ),
+                      ),
+                    )),
+                    SizedBox(height: 30,)
+
+                  ],
+                ),
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text("Category", style: kTitleTextStyle),
-              ],
-            ),
-            SizedBox(height: 30),
-            Expanded(
-              child: StaggeredGridView.countBuilder(
-                padding: EdgeInsets.all(0),
-                crossAxisCount: 2,
-                itemCount: categories.length,
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 20,
-                itemBuilder: (context, index) {
-                  return Container(
-                    padding: EdgeInsets.all(20),
-                    height: 240,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                      image: DecorationImage(
-                        image: AssetImage(categories[index].image),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+            SizedBox(height: 30,),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeAnimation(1,Text("Category", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[800], fontSize: 20),)),
+                  SizedBox(height: 20,),
+                  FadeAnimation(1.4, Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection:Axis.horizontal,
                       children: <Widget>[
-                        Text(
-                          categories[index].name,
-                          style: kTitleTextStyle,
-                        ),
-                        Text(
-                          '${categories[index].numOfCourses} Courses',
-                          style: TextStyle(
-                            color: kTextColor.withOpacity(.5),
-                          ),
-                        )
+                        makeItem(image:'assets/images/Programming.jpg',title : 'Programming'),
+                        makeItem(image:'assets/images/development.jpg',title : 'Development'),
+                        makeItem(image:'assets/images/design.jpg',title : 'Design'),
+                        makeItem(image:'assets/images/IT.jpg',title : 'IT & Software'),
                       ],
                     ),
-                  );
-                },
-                staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+                  )),
+                  SizedBox(height: 80,),
+                ],
               ),
-            ),
+            )
           ],
+
+        ),
+
+      ),
+    );
+  }
+  Widget makeItem({image, title}) {
+    return AspectRatio(
+      aspectRatio: 1 / 1,
+      child: Container(
+        margin: EdgeInsets.only(right: 15),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+                image: AssetImage(image),
+                fit: BoxFit.cover
+            )
+        ),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                  begin: Alignment.bottomRight,
+                  colors: [
+                    Colors.black.withOpacity(.8),
+                    Colors.black.withOpacity(.2),
+                  ]
+              )
+          ),
+          child: Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(title, style: TextStyle(color: Colors.white, fontSize: 20),),
+          ),
         ),
       ),
     );
